3c9a166ae06117bab7752ebab5f51c6a7413efe3
Mohieddine Chatti <mohieddinechatti@gmail.com>
2020-07-26 20:20:00 +0100

    correct and wrong answer sounds done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:swipable/data/data.dart';
 import 'dart:async';
 import 'package:swipable/components/finalResult.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'styles.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,15 +29,19 @@ class _AppState extends State<App> {
   List data = database;
   int score = 0 ;
   int qstNumber = 0;
-  int timeNow =10;
+  int timeNow =15;
   bool stopCounting = false;
-  Color ansColor =  Colors.black;
+  Color scoreColor =  primary;
+  Color correctAns = Color.fromRGBO(0, 250, 0, 1);
+  Color wrongAns = Color.fromRGBO(255, 0, 0, 1);
+
+  final player = AudioCache();
   @override
   Widget build(BuildContext context) {
-    Timer _timer;
+    player.loadAll(["correct.mp3", "wrong.mp3"]);
     startTimer (){
       const period = Duration(seconds: 1);
-      _timer = Timer.periodic(period,  (timer) {
+      Timer.periodic(period,  (timer) {
         if (timeNow > 0 && !stopCounting){
           setState(() {
             timeNow = timeNow-1;
@@ -49,7 +55,7 @@ class _AppState extends State<App> {
               setState(() {
                 qstNumber++;
               });
-          timeNow = 10;
+          timeNow = 15;
             }else{
               print("last Q");
               Navigator.push(context, MaterialPageRoute(builder: (context) => FinalResult()));
@@ -80,82 +86,134 @@ class _AppState extends State<App> {
 
     return notStarted ? start(): MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
         body: Builder(
           builder: (context)=>Container(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: Center(child: Container(child: Text(data[qstNumber][0]))),
-                ),
-                Expanded(
-                  child: Column(
-                    children: <Widget>[
-                       for (int i =1; i< data[qstNumber].length; i++)
-                         RaisedButton(
-                             onPressed: (){
-                            if (data[qstNumber][i] == data[qstNumber][1]) {
-                            print("correct");
-                            setState(() {
-                            stopCounting = true;
-                            score = score+timeNow;
-                            ansColor = Colors.green;
-                            });
-                            setState(() {
-                            timeNow = 10;
-                            stopCounting = false;
-                            });
+            decoration: BoxDecoration(
+              gradient: (LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color.fromRGBO(50,50 , 7, 1), Color.fromRGBO(0, 60, 61, 1)]
+              ))
+            ),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(0), topRight: Radius.circular(0), bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+                        color: scoreColor,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0 ,10),
+                        child: Center(
+                          child: Text("Score  :  ${score.toString()}",style: TextStyle(
+                            fontSize: 20,
+                          ),),
+                        ),
+                      ),
+                    ),
+
+                  SizedBox(height: 30,),
+                  Expanded(
+                    flex: 2,
+                    child: Center(child: Container(constraints: BoxConstraints.expand(),
+//                        decoration: BoxDecoration(
+//                          borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30), bottomLeft: Radius.circular(0), bottomRight: Radius.circular(0)),
+//                        color:Color.fromRGBO(255, 132, 0, 0.1),),
+                      child: Center(child: Text(data[qstNumber][0],
+                    style: TextStyle(
+                      color: qstTextColor,
+                      fontSize: 28
+                    ),),
+                    ))),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                         for (int i =1; i< data[qstNumber].length; i++)
+                           MaterialButton(
+                             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                             color: primary,
+                               minWidth: MediaQuery.of(context).size.width*2/3,
+                               onPressed: (){
+                              if (data[qstNumber][i] == data[qstNumber][1]) {
+                              print("correct");
+                              player.play("correct.mp3");
+                              setState(() {
+                              stopCounting = true;
+                              score = score+timeNow;
+                              scoreColor = correctAns;
+                              });
+                              setState(() {
+                              timeNow = 15;
+                              stopCounting = false;
+                              });
 //                           Scaffold.of(context).showSnackBar(SnackBar(
 //                             content: Text("correct"),
 //                           ));
-                            if(qstNumber <data.length-1){
-                              setState(() {
-                              qstNumber++;
-                              });
-//                              startTimer();
-                            }else{
-                              print("last Q");
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => FinalResult()));
-
-                            }
-
-                            }else{
-                            print("wrong");
-                            setState(() {
-                            stopCounting = true;
-                            score = score-timeNow;
-                            ansColor = Colors.red;
-                            });
-                            setState(() {
-                            timeNow = 10;
-                            stopCounting = false;
-                            });
-                            if(qstNumber <data.length-1) {
-                              setState(() {
+                              if(qstNumber <data.length-1){
+                                setState(() {
                                 qstNumber++;
-                              });
-                            }else{
-                              print("last Q");
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => FinalResult()));
+                                });
+//                              startTimer();
+                              }else{
+                                print("last Q");
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => FinalResult()));
 
-                            }
+                              }
+
+                              }else{
+                              print("wrong");
+                              player.play("wrong.mp3");
+                              setState(() {
+                              stopCounting = true;
+                              score = score-timeNow;
+                              scoreColor = wrongAns;
+                              });
+                              setState(() {
+                              timeNow = 15;
+                              stopCounting = false;
+                              });
+                              if(qstNumber <data.length-1) {
+                                setState(() {
+                                  qstNumber++;
+                                });
+                              }else{
+                                print("last Q");
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => FinalResult()));
+
+                              }
 //                           Scaffold.of(context).showSnackBar(SnackBar(
 //                             content: Text("Wrong"),
 //                           ));
-                            }}
-                            ,
-                             child: Text(data[qstNumber][i]),
-                         ),
-                      Text(timeNow.toString())
+                              }}
+                              ,
+                               child: Text(data[qstNumber][i]),
+                           ),
 
-                    ],
+
+                      ],
+                    ),
                   ),
-                ),
-                Text(score.toString(),style: TextStyle(
-                  color: ansColor,
-                  fontSize: 40,
-                ),)
-              ],
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: primary,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(timeNow.toString()),
+                    ),
+                  )
+
+
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color.fromRGBO(54, 168, 164, 1);
+Color bgColor =  Color.fromRGBO(56, 55, 35,1);
+Color qstTextColor = Color.fromRGBO(247, 236, 183, 1);
